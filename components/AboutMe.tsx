a646ceabb3ec4e7695674f8a10fc64a3285e0454
Xinrui Chen <xyn.hello@gmail.com>
2022-10-11 13:43:58 -0700

    update about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,24 +1,23 @@
-import type { NextPage } from 'next'
-import React, { useCallback, useEffect } from 'react'
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import Image from '@tiptap/extension-image'
-import Link from '@tiptap/extension-link'
-import TextStyle from '@tiptap/extension-text-style'
-import { Color } from '@tiptap/extension-color'
-import { useMediaQuery } from 'react-responsive'
+import type { NextPage } from "next"
+import React, { useCallback, useEffect } from "react"
+import { useEditor, EditorContent } from "@tiptap/react"
+import StarterKit from "@tiptap/starter-kit"
+import Image from "@tiptap/extension-image"
+import Link from "@tiptap/extension-link"
+import TextStyle from "@tiptap/extension-text-style"
+import { Color } from "@tiptap/extension-color"
+import { useMediaQuery } from "react-responsive"
 
 export const aboutMeContent = `
-<h1>hi, i'm xinrui~<br> pronounced <i>/sin - ree/</i>.</h1>
+<h2>hi, i'm xinrui~<br> pronounced <i>/sin - ree/</i>.</h2>
 <p>
-  i'm an artist & developer based in <b>seattle, wa!</b> 
+  i'm an artist & ux developer based in <b>seattle, wa!</b> 
 </p>
 <hr>*
 **<br>
 <br>
-rolling from burger to brewery to bahn mi<br>
-on my $100 bike off craigslist<br>
-doing the city stuff <br>
+biking from burger to brewery to bahn mi<br>
+doing the seattle stuff w/ <a href="https://zane.town/">zane</a><br>
 wiggle waggle <br>
 <br>
 **
@@ -33,31 +32,30 @@ wiggle waggle <br>
 `
 
 const AboutMe: NextPage = () => {
-
-  const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" })
 
   const aboutMeEditor = useEditor({
     editorProps: {
       attributes: {
-        class: 'flex-0',
+        class: "flex-0",
       },
     },
-      content: aboutMeContent,
-  extensions: [
-    StarterKit.configure({
-      heading: {
-        levels: [1,2,3]
-      }
-    }),
-    Image,
-    Link,
-    TextStyle, 
-    Color
-  ],
+    content: aboutMeContent,
+    extensions: [
+      StarterKit.configure({
+        heading: {
+          levels: [1, 2, 3],
+        },
+      }),
+      Image,
+      Link,
+      TextStyle,
+      Color,
+    ],
   })
 
   const addImage = useCallback(() => {
-    const url = window.prompt('Image URL Here')
+    const url = window.prompt("Image URL Here")
 
     if (url) {
       aboutMeEditor.chain().focus().setImage({ src: url }).run()
@@ -68,41 +66,99 @@ const AboutMe: NextPage = () => {
     return null
   }
 
-
   return (
-    <div className={`${isMobile ?'w-7/8':'editor-width-browser'} left-0 right-0 m-auto`}>
+    <div className={`${isMobile ? "w-7/8" : "editor-width-browser"} left-0 right-0 m-auto`}>
+      <div className={`flex-col ${isMobile ? "p-1 mt-3" : "p-20"} rounded-lg`}>
+        {isMobile ? (
+          ""
+        ) : (
+          <div className="editpanel bg-stone-50 drop-shadow-md flex border rounded-lg border-stone-500 m-4">
+            <input
+              type="color"
+              className="w-8 m-4"
+              onInput={(event) =>
+                aboutMeEditor
+                  .chain()
+                  .focus()
+                  .setColor((event.target as HTMLInputElement).value)
+                  .run()
+              }
+            />
 
-    <div className={`flex-col ${isMobile? 'p-1 mt-3':'p-20'} rounded-lg`}>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleBold().run()}
+              className=" p-4 "
+            >
+              <img width="11" src="/bold.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleItalic().run()}
+              className=" p-4 "
+            >
+              <img width="17" src="/italic.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleHeading({ level: 1 }).run()}
+              className=" p-4 "
+            >
+              <img width="17" src="/h1.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleHeading({ level: 2 }).run()}
+              className=" p-4 "
+            >
+              <img width="27" src="/h2.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleBulletList().run()}
+              className=" p-4 "
+            >
+              <img width="17" src="/ul.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleOrderedList().run()}
+              className=" p-4 "
+            >
+              <img width="17" src="/ol.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleCodeBlock().run()}
+              className=" p-4 "
+            >
+              <img width="25" src="/code.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().toggleBlockquote().run()}
+              className=" p-4 "
+            >
+              <img width="16" src="/quote.svg" />
+            </button>
+            <button
+              onClick={() => aboutMeEditor?.chain().focus().setHorizontalRule().run()}
+              className=" p-4 "
+            >
+              <img width="25" src="/hrule.svg" />
+            </button>
+            <button onClick={addImage} className=" p-4 ">
+              <img width="20" src="/image.svg" />
+            </button>
+            <button onClick={() => aboutMeEditor?.chain().focus().undo().run()} className=" p-4 ">
+              <img width="17" src="/undo.svg" />
+            </button>
+            <button onClick={() => aboutMeEditor?.chain().focus().redo().run()} className=" p-4 ">
+              <img width="17" src="/redo.svg" />
+            </button>
+          </div>
+        )}
 
-      {isMobile? '': <div className='editpanel bg-stone-50 drop-shadow-md flex border rounded-lg border-stone-500 m-4'>
-        <input
-          type="color"
-          className= 'w-8 m-4'
-          onInput={event => aboutMeEditor.chain().focus().setColor((event.target as HTMLInputElement).value).run()}
+        <EditorContent
+          editor={aboutMeEditor}
+          spellCheck="false"
+          className="nofocus ml-4 mr-4 border border-stone-500 rounded-lg p-10 bg-stone-50 drop-shadow-lg"
         />
-
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleBold().run()} className=' p-4 '>< img width='11' src='/bold.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleItalic().run()} className=' p-4 '>< img width='17' src='/italic.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleHeading({ level: 1 }).run()} className=' p-4 '>< img width='17' src='/h1.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleHeading({ level: 2 }).run()} className=' p-4 '>< img width='27' src='/h2.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleBulletList().run()} className=' p-4 '>< img width='17' src='/ul.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleOrderedList().run()} className=' p-4 '>< img width='17' src='/ol.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleCodeBlock().run()} className=' p-4 '>< img width='25' src='/code.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().toggleBlockquote().run()} className=' p-4 '>< img width='16' src='/quote.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().setHorizontalRule().run()} className=' p-4 '>< img width='25' src='/hrule.svg'/></button>
-        <button onClick={addImage} className=' p-4 '>< img width='20' src='/image.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().undo().run()} className=' p-4 '>< img width='17' src='/undo.svg'/></button>
-        <button onClick={() => aboutMeEditor?.chain().focus().redo().run()} className=' p-4 '>< img width='17' src='/redo.svg'/></button>
-      </div>}
-
-      <EditorContent editor={aboutMeEditor} spellCheck="false" className='nofocus ml-4 mr-4 border border-stone-500 rounded-lg p-10 bg-stone-50 drop-shadow-lg'/>
-
-
       </div>
-
-      </div>
-
-  );
+    </div>
+  )
 }
 
-export default AboutMe;
+export default AboutMe
